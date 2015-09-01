408468d9b9a0fb0c6a2bbf4fc9bd02b406ced73b
smalldu <dz@duzhedeMacBook-Pro.local>
2015-09-01 20:18:01 +0800

    original

---
+++ MasonryDemo/MasonryDemo2/MasonryDemo2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MasonryDemo2
+//
+//  Created by duzhe on 15/9/1.
+//  Copyright © 2015年 duzhe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
