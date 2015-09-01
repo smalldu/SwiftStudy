938bdd75695467fce5a94d3c23b16dfdf232aacb
smalldu <dz@duzhedeMacBook-Pro.local>
2015-09-01 23:46:23 +0800

    new add Snap

--- MasonryDemo/MasonryDemo2/MasonryDemo2/ViewController.swift
+++ MasonryDemo/MasonryDemo2/MasonryDemo2/ViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
-
+import SnapKit
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lbl1: UILabel!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let view1 = UIView()
+        view1.translatesAutoresizingMaskIntoConstraints = true;
+        view1.backgroundColor = UIColor.redColor()
+        self.view.addSubview(view1)
+        self.view.sendSubviewToBack(view1)
+        let padding = UIEdgeInsetsMake(10, 10, 10, 10);
+        
+        view1.snp_makeConstraints { (make:ConstraintMaker) -> Void in
+            make.edges.equalTo(self.view).inset(padding)
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
